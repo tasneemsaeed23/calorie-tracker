eb94f546e100a16c0a6e00ae7ea7785f7397d052
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-05-06 12:01:55 +0300

    Filter by date

--- src/components/CalorieRecordsSection/ListingSection.jsx
+++ src/components/CalorieRecordsSection/ListingSection.jsx
@@ -1,7 +1,39 @@
 import RecordList from "./RecordList";
+import styles from "./ListingSection.module.css";
+import { useState } from "react";
+
 function ListingSection(props) {
   const { allRecords } = props;
-  return <RecordList records={allRecords} />;
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  const dateChangeHandler = (event) => {
+    setCurrentDate(new Date(event.target.value));
+  };
+
+  const dateFilter = (record) => {
+    const recordDate = new Date(record.date);
+    return (
+      recordDate.getDate() === currentDate.getDate() &&
+      recordDate.getMonth() === currentDate.getMonth() &&
+      recordDate.getFullYear() === currentDate.getFullYear()
+    );
+  };
+
+  return (
+    <>
+      <label className={styles["listing-picker-label"]} htmlFor="listingDate">
+        Select date
+      </label>
+      <input
+        id="listingDate"
+        className={styles["listing-picker-input"]}
+        type="date"
+        value={currentDate.toISOString().split("T")[0]}
+        onChange={dateChangeHandler}
+      />
+      <RecordList records={allRecords.filter(dateFilter)} />
+    </>
+  );
 }
 
 export default ListingSection;
