16b9eaca0f95ae2dbbe37781b9fcd6624f411103
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-05-09 16:23:25 +0300

    Placeholder for empty list

--- src/components/CalorieRecordsSection/RecordList.jsx
+++ src/components/CalorieRecordsSection/RecordList.jsx
@@ -2,19 +2,24 @@ import CalorieRecord from "./CalorieRecord";
 import styles from "./RecordList.module.css";
 
 function RecordList(props) {
-  return (
+  return props.records?.length ? (
     <ul className={styles["record-list"]}>
-      {props.records.map((record) => (
-        <li className={styles["list-Item"]} key={record.id}>
-          <CalorieRecord
-            date={record.date}
-            meal={record.meal}
-            content={record.content}
-            calories={record.calories}
-          />
-        </li>
-      ))}
+      {props.records.map(
+        (record) =>
+          record.calories >= 0 && (
+            <li className={styles["list-Item"]} key={record.id}>
+              <CalorieRecord
+                date={record.date}
+                meal={record.meal}
+                content={record.content}
+                calories={record.calories}
+              />
+            </li>
+          )
+      )}
     </ul>
+  ) : (
+    <div className={styles.placeholder}>No records found for this date</div>
   );
 }
 
