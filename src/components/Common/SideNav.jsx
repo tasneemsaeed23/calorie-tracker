b0faf27e8902ba7b8646384a5219d51d7ed580d0
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-05-30 02:47:22 +0300

    Working with navigation links

--- src/components/Common/SideNav.jsx
+++ src/components/Common/SideNav.jsx
@@ -1,12 +1,22 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styles from "./SideNav.module.css";
 
 function SideNav() {
   return (
     <nav className={styles.nav}>
       <h1 className={styles.title}>Calorie Tracker</h1>
-      <Link to="/">Home</Link>
-      <Link to="/track">Track</Link>
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? styles.active : undefined)}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/track"
+        className={({ isActive }) => (isActive ? styles.active : undefined)}
+      >
+        Track
+      </NavLink>
     </nav>
   );
 }
