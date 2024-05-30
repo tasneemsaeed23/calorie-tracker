f18b775bf856e7aff1d9862a7dc860bc885a77c8
tasneemsaeed23 <tsnymmwsy1@gmail.com>
2024-05-31 01:22:22 +0300

    Index path

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        path: "",
+        index: true,
         element: <LandingPage />,
       },
       {
